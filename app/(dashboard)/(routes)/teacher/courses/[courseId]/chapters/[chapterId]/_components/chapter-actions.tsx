3a391925d2c0d835c86a5e66ad50397c9d8b14ff
Alohadancemeow <nawongsak@gmail.com>
2024-03-18 15:49:55 +0700

    🍀 chapter: publish

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -7,7 +7,7 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
-// import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 interface ChapterActionsProps {
   disabled: boolean;
@@ -56,8 +56,8 @@ export const ChapterActions = ({
       await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
 
       toast.success("Chapter deleted");
-      router.refresh();
       router.push(`/teacher/courses/${courseId}`);
+      router.refresh();
     } catch {
       toast.error("Something went wrong");
     } finally {
@@ -75,11 +75,11 @@ export const ChapterActions = ({
       >
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      {/* <ConfirmModal onConfirm={onDelete}>
+      <ConfirmModal onConfirm={onDelete}>
         <Button size="sm" disabled={isLoading}>
           <Trash className="h-4 w-4" />
         </Button>
-      </ConfirmModal> */}
+      </ConfirmModal>
     </div>
   );
 };
